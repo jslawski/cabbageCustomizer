b7535e3c820259dad2558e73dc1d7b68c27403e9
Jared Slawski <jaredslawski@gmail.com>
2023-09-06 20:54:04 -0600

    Updated server name

--- Assets/_Scripts/TwitchSecrets.cs
+++ Assets/_Scripts/TwitchSecrets.cs
@@ -6,6 +6,6 @@ public static class TwitchSecrets
 {
     public static string ClientID = "x0ibfu57j1tldduiqrehqr9ttjh787";
     public static string ClientSecret = "uo15n1waan7ipkgmd9vkzk0dqb7c8f";
-    public static string ServerName = "https://18.190.87.149";
-    public static string RedirectURL = "https://18.190.87.149/authComplete.php";
+    public static string ServerName = "https://cabbagecustomizer.net";
+    public static string RedirectURL = "https://cabbagecustomizer.net/authComplete.php";
 }
